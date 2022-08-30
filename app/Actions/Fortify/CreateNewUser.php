629cb52e75ef8838247292f5a78624dc3e3192c0
haaragard <haaragard@gmail.com>
2022-08-30 01:32:03 -0300

    feat: update create new user to add empty worker profile

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -2,7 +2,11 @@
 
 namespace App\Actions\Fortify;
 
+use App\Exceptions\CreateUserException;
 use App\Models\User;
+use App\Models\Worker;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
@@ -16,9 +20,11 @@ class CreateNewUser implements CreatesNewUsers
      * Validate and create a newly registered user.
      *
      * @param  array  $input
-     * @return \App\Models\User
+     * @return User
+     *
+     * @throws CreateUserException
      */
-    public function create(array $input)
+    public function create(array $input): User
     {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
@@ -27,10 +33,39 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
-            'name' => $input['name'],
-            'email' => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
+        return $this->createNewUser($input);
+    }
+
+    /**
+     * @param  array  $attributes
+     * @return User
+     */
+    private function createNewUser(array $attributes): User
+    {
+        $user = null;
+        DB::transaction(function () use (&$user, &$attributes) {
+            $user = User::create([
+                'name' => $attributes['name'],
+                'email' => $attributes['email'],
+                'password' => Hash::make($attributes['password']),
+            ]);
+
+            $this->createWorkerFromUser($user);
+        });
+
+        if (is_null($user)) {
+            throw new CreateUserException();
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param  User  $user
+     * @return Worker
+     */
+    private function createWorkerFromUser(User $user): Worker
+    {
+        return $user->worker()->create();
     }
 }
