743e99acd6c9031deaf52c20c861348bbcd5d02a
haaragard <haaragard@gmail.com>
2022-08-30 22:58:15 -0300

    migration: remove column cpf from workers table

--- database/migrations/2022_08_29_235612_create_workers_table.php
+++ database/migrations/2022_08_29_235612_create_workers_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('workers', function (Blueprint $table) {
             $table->id('id');
             $table->foreignId('user_id');
-            $table->string('cpf', 11)->nullable()->unique();
             $table->string('cnpj', 14)->nullable()->unique();
             $table->timestamps();
         });
