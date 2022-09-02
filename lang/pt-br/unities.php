027c8b12895e694c391f6cdf8b16ec4d4bd89d0d
haaragard <haaragard@gmail.com>
2022-09-01 22:51:02 -0300

    feat: add unities lang translation

---
+++ lang/pt-br/unities.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Unities Language Variations
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'square meter' => 'metro quadrado',
+    'square centimeter' => 'centímetro quadrado',
+    'minute' => 'minuto',
+    'hour' => 'hora',
+    'item' => 'item',
+
+];
