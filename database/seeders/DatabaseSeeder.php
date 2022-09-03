fea5dce74cf8ddd03575bec4fd0f611ee2d62760
haaragard <haaragard@gmail.com>
2022-09-03 04:05:02 -0300

    feat: add works seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             WorkerSeeder::class,
             ContractorSeeder::class,
+            WorkSeeder::class,
         ]);
     }
 }
