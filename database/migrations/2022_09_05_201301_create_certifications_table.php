1f2caadd3dc4cd80c2ef9a56dd0d37d2d4fc6917
haaragard <haaragard@gmail.com>
2022-09-05 20:20:07 -0300

    migration: create certifications table

---
+++ database/migrations/2022_09_05_201301_create_certifications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certifications', function (Blueprint $table) {
+            $table->id('id');
+            $table->uuid('uuid');
+            $table->foreignId('worker_id');
+            $table->string('name');
+            $table->string('description', 1024)->nullable();
+            $table->char('certified_uuid', 100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certifications');
+    }
+};
