9a9ef1bd1df2788a28f609157b614051c91a5e20
haaragard <haaragard@gmail.com>
2022-09-09 02:00:44 -0300

    feat: add logo component

---
+++ resources/js/Components/Logos/Logo.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <div class="w-fit">
+        <Link :href="route('landingpage')">
+            <h1 class="font-bold text-3xl">
+                GetWorkers
+            </h1>
+        </Link>
+    </div>
+</template>
