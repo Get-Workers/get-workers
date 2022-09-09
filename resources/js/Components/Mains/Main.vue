4093f4791c4c03e14083bf7313c3332eed19fff1
haaragard <haaragard@gmail.com>
2022-09-09 02:01:43 -0300

    feat: add main component

---
+++ resources/js/Components/Mains/Main.vue
@@ -0,0 +1,9 @@
+<template>
+    <main>
+        <div class="pt-24 pb-6 min-h-screen">
+            <div class="container mx-auto bg-gray-100 rounded p-5">
+                <slot />
+            </div>
+        </div>
+    </main>
+</template>
