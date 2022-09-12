1e4d2a54e93115a97f9592e30a22f65b6c4f1921
haaragard <haaragard@gmail.com>
2022-09-12 00:49:49 -0300

    feat: add user register action

--- app/Actions/Auth/CreateNewUser.php
+++ app/Actions/Auth/CreateNewUser.php
@@ -1,21 +1,16 @@
 <?php
 
-namespace App\Actions\Fortify;
+namespace App\Actions\Auth;
 
 use App\Exceptions\CreateUserException;
 use App\Models\User;
 use App\Models\Worker;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
-use Laravel\Jetstream\Jetstream;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
-
     /**
      * Validate and create a newly registered user.
      *
@@ -26,13 +21,6 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => $this->passwordRules(),
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-        ])->validate();
-
         return $this->createNewUser($input);
     }
 
