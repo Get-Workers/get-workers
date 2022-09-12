6443c09ece05aa718792db1a0ba94f2221e61592
haaragard <haaragard@gmail.com>
2022-09-12 00:51:02 -0300

    feat: added layout and changed default register form

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,12 +1,13 @@
 <script setup>
-import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
-import JetAuthenticationCard from '@/Components/AuthenticationCard.vue';
-import JetAuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
-import JetButton from '@/Components/Button.vue';
-import JetInput from '@/Components/Input.vue';
-import JetInputError from '@/Components/InputError.vue';
-import JetCheckbox from '@/Components/Checkbox.vue';
-import JetLabel from '@/Components/Label.vue';
+import { useForm } from '@inertiajs/inertia-vue3';
+import AuthenticationCard from '@/Components/AuthenticationCard.vue';
+import Button from '@/Components/Button.vue';
+import Input from '@/Components/Input.vue';
+import InputError from '@/Components/InputError.vue';
+import Checkbox from '@/Components/Checkbox.vue';
+import Label from '@/Components/Label.vue';
+import GuestLayout from '../../Layouts/GuestLayout.vue';
+import Link from '../../Components/Links/Link.vue';
 
 const form = useForm({
     name: '',
@@ -24,88 +25,102 @@ const submit = () => {
 </script>
 
 <template>
-    <Head title="Register" />
-
-    <JetAuthenticationCard>
-        <template #logo>
-            <JetAuthenticationCardLogo />
-        </template>
-
-        <form @submit.prevent="submit">
-            <div>
-                <JetLabel for="name" value="Name" />
-                <JetInput
-                    id="name"
-                    v-model="form.name"
-                    type="text"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                    autocomplete="name"
-                />
-                <JetInputError class="mt-2" :message="form.errors.name" />
-            </div>
-
-            <div class="mt-4">
-                <JetLabel for="email" value="Email" />
-                <JetInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                />
-                <JetInputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <JetLabel for="password" value="Password" />
-                <JetInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="new-password"
-                />
-                <JetInputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4">
-                <JetLabel for="password_confirmation" value="Confirm Password" />
-                <JetInput
-                    id="password_confirmation"
-                    v-model="form.password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="new-password"
-                />
-                <JetInputError class="mt-2" :message="form.errors.password_confirmation" />
-            </div>
-
-            <div v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature" class="mt-4">
-                <JetLabel for="terms">
-                    <div class="flex items-center">
-                        <JetCheckbox id="terms" v-model:checked="form.terms" name="terms" required />
-
-                        <div class="ml-2">
-                            I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
-                        </div>
+    <GuestLayout title="Register">
+        <template #main>
+            <!-- Register -->
+            <AuthenticationCard>
+                <div>
+                    <span class="text-2xl font-semibold">Register</span>
+                    <div>
+                        <span class="inline-block">Already have an account?</span>
+                        <Link class="inline-block ml-2" :href="route('login')">Login</Link>
                     </div>
-                    <JetInputError class="mt-2" :message="form.errors.terms" />
-                </JetLabel>
-            </div>
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <Link :href="route('login')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Already registered?
-                </Link>
+                <div class="mt-2">
+                    <form @submit.prevent="submit">
+                        <div class="space-y-3">
+                            <div>
+                                <Label for="name" value="Name" />
+                                <Input
+                                    id="name"
+                                    v-model="form.name"
+                                    type="text"
+                                    class="mt-1 block w-full"
+                                    required
+                                    autofocus
+                                    autocomplete="name"
+                                    placeholder="Full name"
+                                />
+                                <InputError class="mt-2" :message="form.errors.name" />
+                            </div>
 
-                <JetButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </JetButton>
-            </div>
-        </form>
-    </JetAuthenticationCard>
+                            <div>
+                                <Label for="email" value="Email" />
+                                <Input
+                                    id="email"
+                                    v-model="form.email"
+                                    type="email"
+                                    class="mt-1 block w-full"
+                                    required
+                                    autofocus
+                                    autocomplete="email"
+                                    placeholder="Email"
+                                />
+                                <InputError class="mt-2" :message="form.errors.email" />
+                            </div>
+
+                            <div>
+                                <Label for="password" value="Password" />
+                                <Input
+                                    id="password"
+                                    v-model="form.password"
+                                    type="password"
+                                    class="mt-1 block w-full"
+                                    required
+                                    autofocus
+                                    autocomplete="password"
+                                    placeholder="Password"
+                                />
+                                <InputError class="mt-2" :message="form.errors.password" />
+                            </div>
+
+                            <div>
+                                <Label for="password_confirmation" value="Confirm password" />
+                                <Input
+                                    id="password_confirmation"
+                                    v-model="form.password_confirmation"
+                                    type="password"
+                                    class="mt-1 block w-full"
+                                    required
+                                    autofocus
+                                    autocomplete="password"
+                                    placeholder="Confirm password"
+                                />
+                                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                            </div>
+                        </div>
+
+                        <div v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature" class="mt-4">
+                            <Label for="terms">
+                                <div class="flex items-center">
+                                    <Checkbox id="terms" v-model:checked="form.terms" name="terms" required />
+                                    <div class="ml-2">
+                                        I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900">Privacy Policy</a>
+                                    </div>
+                                </div>
+                                <InputError class="mt-2" :message="form.errors.terms" />
+                            </Label>
+                        </div>
+
+                        <div class="flex items-center justify-end mt-8">
+                            <Button class="mx-auto w-full text-center" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                                Register
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            </AuthenticationCard>
+        </template>
+    </GuestLayout>
 </template>
