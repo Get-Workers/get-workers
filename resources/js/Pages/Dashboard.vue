30fae93830a350c538c4d19f3e7a85bd6acd0e1a
haaragard <haaragard@gmail.com>
2022-09-12 21:25:28 -0300

    feat: change dashboard layout to auth

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,13 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import AuthLayout from '../Layouts/AuthLayout.vue';
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+    <AuthLayout title="Welcome">
+        <template #main>
+            <div class="h-96">
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
             </div>
-        </div>
-    </AppLayout>
+        </template>
+    </AuthLayout>
 </template>
