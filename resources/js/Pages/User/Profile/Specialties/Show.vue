159b7d7f91c1ba8bd413d99b1c2ce5d53f283e22
haaragard <haaragard@gmail.com>
2022-09-14 00:00:14 -0300

    chore: add user profile specialties show component

---
+++ resources/js/Pages/User/Profile/Specialties/Show.vue
@@ -0,0 +1,29 @@
+<script setup>
+import AuthLayout from '../../../../Layouts/AuthLayout.vue';
+import SidebarMenu from '../Partials/SidebarMenu.vue';
+
+defineProps({
+    specialties: {
+        type: Array,
+        required: true,
+        default: []
+    }
+})
+</script>
+
+<template>
+    <AuthLayout title="Specialties">
+        <template #main>
+            <div class="flex">
+                <!-- Sidebard Menu -->
+                <SidebarMenu class="flex flex-col h-fit w-64 md:w-96"/>
+
+                <!-- Page Content -->
+                <div class="ml-5 w-full">
+                    asd
+                </div>
+            </div>
+
+        </template>
+    </AuthLayout>
+</template>
