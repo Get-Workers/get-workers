35c1b1bf66344d2b1e2745a627f76c3e9c286900
haaragard <haaragard@gmail.com>
2022-09-13 21:32:46 -0300

    fix: change guest landingpage name order

--- routes/web/guest.php
+++ routes/web/guest.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\Auth\UserRegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::name('landingpage')->get('/', function () {
+    Route::get('/', function () {
         return inertia('Landingpage');
-    });
+    })->name('landingpage');
 
     Route::prefix('/register')->name('register')->group(function () {
         Route::get('', RegisterController::class);
