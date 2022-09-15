7e44e21ed6a08fd4d4ba4fe40ede18496bd6d43a
haaragard <haaragard@gmail.com>
2022-09-15 18:55:47 -0300

    feat: add worker profile middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'worker-profile' => \App\Http\Middleware\WorkerProfile::class,
     ];
 }
