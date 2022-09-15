c45e2aab6f09396d45c6c7891cefd66919653b71
haaragard <haaragard@gmail.com>
2022-09-14 23:50:58 -0300

    feat: add destroy specialty request for worker profile

---
+++ app/Http/Requests/User/Profile/Worker/DestroySpecialtyRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\User\Profile\Worker;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DestroySpecialtyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return (! is_null(auth()->user()->worker));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'specialtyId' => 'numeric|exists:specialty_worker,specialty_id'
+        ];
+    }
+}
