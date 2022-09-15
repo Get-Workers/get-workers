8417556af76606a7d7fba1c8ca27c78319bde5ab
haaragard <haaragard@gmail.com>
2022-09-15 18:55:55 -0300

    feat: add contractor profile middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'worker-profile' => \App\Http\Middleware\WorkerProfile::class,
+        'contractor-profile' => \App\Http\Middleware\ContractorProfile::class,
     ];
 }
