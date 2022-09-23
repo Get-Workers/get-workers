f9c0366521689a2c0d0315ac837364846aba8b3c
haaragard <haaragard@gmail.com>
2022-09-22 21:43:14 -0300

    feat: add icons into auth header component

--- resources/js/Components/Headers/AuthHeader.vue
+++ resources/js/Components/Headers/AuthHeader.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed } from '@vue/reactivity';
 import { Inertia } from '@inertiajs/inertia';
-import { Link } from '@inertiajs/inertia-vue3';
+import { AccountCircle, MenuDown } from 'mdue';
 import Header from './Header.vue';
 import Dropdown from '../../Components/Dropdown.vue';
 import DropdownLink from '../../Components/DropdownLink.vue';
@@ -35,16 +35,11 @@ const logout = () => {
 
                         <span v-else class="inline-flex rounded-md">
                             <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                <AccountCircle class="text-3xl mr-2"/>
+
                                 {{ $page.props.user.name }}
 
-                                <svg
-                                    class="ml-2 -mr-0.5 h-4 w-4"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                >
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
+                                <MenuDown class="ml-2 -mr-0.5 h-4 w-4"/>
                             </button>
                         </span>
                     </template>
