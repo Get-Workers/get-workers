fa05eb4ab053285632cf41b1209ab1bd59737408
haaragard <haaragard@gmail.com>
2022-10-03 20:26:22 -0300

    feat: add specialty cache service

---
+++ app/Services/Caches/SpecialtyCacheService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services\Caches;
+
+use App\Models\Specialty;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Cache;
+
+class SpecialtyCacheService
+{
+    /**
+     * @param  bool  $clear
+     *
+     * @return Collection|null
+     */
+    public static function all(bool $clear = false): ?Collection
+    {
+        $key = 'specialty:all';
+
+        if ($clear) {
+            Cache::forget($key);
+            return null;
+        }
+
+        return Cache::rememberForever($key, fn () => Specialty::all());
+    }
+}
