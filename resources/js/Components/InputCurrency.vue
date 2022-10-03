d962747eebfa25c702067dace3533dc3d19acc8d
haaragard <haaragard@gmail.com>
2022-10-03 20:23:44 -0300

    feat: add new input currency component

---
+++ resources/js/Components/InputCurrency.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useCurrencyInput } from 'vue-currency-input';
+
+const props = defineProps({
+    modelValue: Number,
+    options: Object,
+    disabled: {
+        type: Boolean,
+        default: false
+    }
+});
+
+defineEmits(['update:modelValue']);
+
+const { inputRef, numberValue } = useCurrencyInput(props.options, false);
+</script>
+
+<template>
+    <input
+        ref="inputRef"
+        type="text"
+        class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+        :class="{ 'bg-gray-200': disabled }"
+        :disabled="disabled"
+        :value="modelValue"
+        @input="$emit('update:modelValue', numberValue)"
+    >
+</template>
