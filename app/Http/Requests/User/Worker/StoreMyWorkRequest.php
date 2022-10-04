914bec6b0a08c3854e31fcbd30ebe8176066c7cb
haaragard <haaragard@gmail.com>
2022-10-03 22:07:08 -0300

    feat: add store my works request

---
+++ app/Http/Requests/User/Worker/StoreMyWorkRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\User\Worker;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreMyWorkRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return (! is_null(auth()->user()->worker));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|min:5|max:255',
+            'slug' => 'required|string|min:5|max:255',
+            'time' => 'nullable|time',
+            'price' => 'nullable|numeric',
+            'has_unity' => 'nullable|boolean',
+            'unity_id' => 'nullable|integer|required_if:has_unity,true|exists:unities,id',
+            'specialties' => 'nullable|array|exists:unities,id',
+        ];
+    }
+}
