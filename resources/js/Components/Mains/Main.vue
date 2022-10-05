0920a85678ab5ef5d63b8954848667f7efeb90ba
haaragard <haaragard@gmail.com>
2022-10-05 00:14:41 -0300

    feat: add class control for minor width screens

--- resources/js/Components/Mains/Main.vue
+++ resources/js/Components/Mains/Main.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
         <div class="pt-24 pb-6 min-h-screen">
-            <div class="container mx-auto bg-gray-100 rounded p-5">
+            <div class="container mx-auto bg-gray-100 rounded py-5 sm:p-5">
                 <slot />
             </div>
         </div>
