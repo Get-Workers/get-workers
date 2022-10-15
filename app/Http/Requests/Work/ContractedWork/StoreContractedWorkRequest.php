75787a99c9a686d4779dcae7ddd69d9618cb6d20
haaragard <haaragard@gmail.com>
2022-10-15 17:29:07 -0300

    feat: add store contracted work request

---
+++ app/Http/Requests/Work/ContractedWork/StoreContractedWorkRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Work\ContractedWork;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreContractedWorkRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $user = auth()->user();
+        if (is_null(auth()->user())) {
+            return false;
+        }
+
+        return (! is_null($user->contractor));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'work' => 'required|exists:works,uuid',
+        ];
+    }
+}
