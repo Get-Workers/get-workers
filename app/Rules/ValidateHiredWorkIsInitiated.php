b64ee35eefa02bde4ab93222327bd8bd2e50a519
haaragard <haaragard@gmail.com>
2022-10-18 00:35:09 -0300

    feat: add validation rule for hired work initiated

---
+++ app/Rules/ValidateHiredWorkIsInitiated.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Rules;
+
+use App\Models\HiredWork;
+use Illuminate\Contracts\Validation\InvokableRule;
+
+class ValidateHiredWorkIsInitiated implements InvokableRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @return void
+     */
+    public function __invoke($attribute, $value, $fail)
+    {
+        $hiredWork = HiredWork::whereUuid($value)->firstOrFail();
+
+        if (! is_null($hiredWork->initiated_at)) {
+            $fail('validation.hired_work_is_initiated')->translate();
+        }
+    }
+}
