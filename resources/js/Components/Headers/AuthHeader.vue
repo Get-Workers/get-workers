e1f83f5d6259163d4e4a14748e9e6dc01f19fdc3
haaragard <haaragard@gmail.com>
2022-10-19 21:56:56 -0300

    refactor: improve code on auth header

--- resources/js/Components/Headers/AuthHeader.vue
+++ resources/js/Components/Headers/AuthHeader.vue
@@ -9,18 +9,59 @@ import DropdownLink from '../../Components/DropdownLink.vue';
 import MenuHeaderX from '../Menus/MenuHeaderX.vue';
 import ItemX from '../Menus/Items/ItemX.vue';
 
-const dashboardRoute = 'dashboard';
-const myWorksRoute = 'user.worker.my-works.show';
-const worksRoute = 'works.list';
-const hiredWorksRoute = 'hired-works.list';
+const pageProps = usePage().props.value;
+const currentRoute = computed(() => route().current());
 
-const isDashboardRoute = computed(() => (route().current() === dashboardRoute));
-const isMyWorksRoute = computed(() => (route().current() === myWorksRoute));
-const isWorksRoute = computed(() => (route().current() === worksRoute));
-const isHiredWorksRoute = computed(() => (route().current() === hiredWorksRoute));
+const isWorker = (pageProps.worker !== null);
+const isContractor = (pageProps.contractor !== null);
+const canSee = {
+    worker: isWorker,
+    contractor: isContractor,
+};
 
-const showWorkerMenu = computed(() => (usePage().props.value.worker !== null));
-const showContractorMenu = computed(() => (usePage().props.value.contractor !== null));
+const routes = {
+    // Main
+    dashboard: 'dashboard',
+
+    // Worker
+    worker: {
+        myWorks: 'user.worker.my-works.show',
+    },
+
+    // Contractor
+    contractor: {
+        hiredWorks: {
+            list: 'hired-works.list',
+        },
+    },
+
+    // Works
+    works: {
+        list: 'works.list'
+    },
+};
+
+const isRoute = computed(() => ({
+    // Main
+    dashboard: (currentRoute.value === routes.dashboard),
+
+    // Worker
+    worker: {
+        myWorks: (currentRoute.value === routes.worker.myWorks),
+    },
+
+    // Contractor
+    contractor: {
+        hiredWorks: {
+            list: (currentRoute.value === routes.contractor.hiredWorks.list),
+        },
+    },
+
+    // Works
+    works: {
+        list: (currentRoute.value === routes.works.list),
+    },
+}));
 
 const logout = () => {
     Inertia.post(route('logout'));
@@ -31,33 +72,33 @@ const logout = () => {
     <Header>
         <template #links>
             <MenuHeaderX class="h-full">
-                <ItemX class="h-full hover:bg-white" :class="{ 'bg-white': isDashboardRoute }">
-                    <template v-if="! isDashboardRoute">
-                        <Link :href="route(dashboardRoute)" class="flex h-full px-3 items-center">Dashboard</Link>
+                <ItemX class="h-full hover:bg-white" :class="{ 'bg-white': isRoute.dashboard }">
+                    <template v-if="! isRoute.dashboard">
+                        <Link :href="route(routes.dashboard)" class="flex h-full px-3 items-center">Dashboard</Link>
                     </template>
                     <template v-else>
                         <span class="flex h-full px-3 items-center">Dashboard</span>
                     </template>
                 </ItemX>
-                <ItemX class="h-full hover:bg-white border-l" :class="{ 'bg-white': isMyWorksRoute }" v-if="showWorkerMenu">
-                    <template v-if="! isMyWorksRoute">
-                        <Link :href="route(myWorksRoute)" class="flex h-full px-3 items-center">My Works</Link>
+                <ItemX class="h-full hover:bg-white border-l" :class="{ 'bg-white': isRoute.worker.myWorks }" v-if="canSee.worker">
+                    <template v-if="! isRoute.worker.myWorks">
+                        <Link :href="route(routes.worker.myWorks)" class="flex h-full px-3 items-center">My Works</Link>
                     </template>
                     <template v-else>
                         <span class="flex h-full px-3 items-center">My Works</span>
                     </template>
                 </ItemX>
-                <ItemX class="h-full hover:bg-white border-l" :class="{ 'bg-white': isWorksRoute }" v-if="showContractorMenu">
-                    <template v-if="! isWorksRoute">
-                        <Link :href="route(worksRoute)" class="flex h-full px-3 items-center">Works</Link>
+                <ItemX class="h-full hover:bg-white border-l" :class="{ 'bg-white': isRoute.works.list }">
+                    <template v-if="! isRoute.works.list">
+                        <Link :href="route(routes.works.list)" class="flex h-full px-3 items-center">Works</Link>
                     </template>
                     <template v-else>
                         <span class="flex h-full px-3 items-center">Works</span>
                     </template>
                 </ItemX>
-                <ItemX class="h-full hover:bg-white border-l" :class="{ 'bg-white': isHiredWorksRoute }" v-if="showContractorMenu">
-                    <template v-if="! isHiredWorksRoute">
-                        <Link :href="route(hiredWorksRoute)" class="flex h-full px-3 items-center">Hired Works</Link>
+                <ItemX class="h-full hover:bg-white border-l" :class="{ 'bg-white': isRoute.contractor.hiredWorks.list }" v-if="canSee.contractor">
+                    <template v-if="! isRoute.contractor.hiredWorks.list">
+                        <Link :href="route(routes.contractor.hiredWorks.list)" class="flex h-full px-3 items-center">Hired Works</Link>
                     </template>
                     <template v-else>
                         <span class="flex h-full px-3 items-center">Hired Works</span>
