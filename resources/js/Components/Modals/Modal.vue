2ed49dd9e35547aa462b312fba883804cca877e3
haaragard <haaragard@gmail.com>
2022-10-25 00:32:22 -0300

    feat: add modal component

---
+++ resources/js/Components/Modals/Modal.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { onMounted, onUpdated, ref } from 'vue';
+import { Close } from 'mdue';
+
+const props = defineProps({
+    title: String,
+    isOpened: Boolean,
+    onClose: Function
+});
+
+const modal = ref(null);
+
+onMounted(() => openModal());
+onUpdated(() => openModal());
+
+function openModal() {
+    if (!props.isOpened && !modal.value.open) return;
+
+    modal.value.showModal();
+}
+
+function closeModal() {
+    props.onClose();
+    modal.value.close();
+}
+
+</script>
+
+<template>
+    <teleport to="body">
+        <transition leave-active-class="duration-500">
+            <dialog class="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-full w-full" ref="modal" @close.prevent="closeModal">
+                <div class="relative" v-if="isOpened">
+                    <span class="text-xl font-bold">{{ title }}</span>
+
+                    <div class="absolute -right-2 -top-2 border rounded-full w-8 h-8 cursor-pointer flex items-center justify-center hover:bg-blue-500 hover:text-white">
+                        <Close @click="closeModal"/>
+                    </div>
+
+                </div>
+                <div class="mt-2"><slot /></div>
+            </dialog>
+        </transition>
+    </teleport>
+</template>
