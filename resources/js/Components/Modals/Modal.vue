1c55e566162ce29026f954c2d6324fda4faa007a
haaragard <haaragard@gmail.com>
2022-10-25 23:39:09 -0300

    chore: improve modal component

--- resources/js/Components/Modals/Modal.vue
+++ resources/js/Components/Modals/Modal.vue
@@ -29,11 +29,11 @@ function closeModal() {
 <template>
     <teleport to="body">
         <transition leave-active-class="duration-500">
-            <dialog class="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-full w-full" ref="modal" @close.prevent="closeModal">
+            <dialog class="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-full w-full self-center rounded break-words" ref="modal" @close.prevent="closeModal">
                 <div class="relative" v-if="isOpened">
                     <span class="text-xl font-bold">{{ title }}</span>
 
-                    <div class="absolute -right-2 -top-2 border rounded-full w-8 h-8 cursor-pointer flex items-center justify-center hover:bg-blue-500 hover:text-white">
+                    <div class="absolute -right-4 -top-4 border rounded-full w-8 h-8 cursor-pointer flex items-center justify-center hover:bg-blue-500 hover:text-white">
                         <Close @click="closeModal"/>
                     </div>
 
