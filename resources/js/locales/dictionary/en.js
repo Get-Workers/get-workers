5d6d15f43eab3ed9a6c7a79089acfcf947a85b8c
haaragard <haaragard@gmail.com>
2022-11-03 19:30:52 -0300

    feat: add first dictionaries and i18n plugin config

---
+++ resources/js/locales/dictionary/en.js
@@ -0,0 +1,13 @@
+const en = {
+    words: {
+        contractor: 'Contractor',
+        worker: 'Worker',
+        hiredWork: 'Hired work',
+        register: 'Register',
+        login: 'Login',
+        lowerCaseOr: 'or',
+    },
+    phrases: {},
+};
+
+export default en;
