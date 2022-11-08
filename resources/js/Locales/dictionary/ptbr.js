80b6e30ff1ce83533422756f4507091e18af5d0b
KlaitonR <klaitonricken@hotmail.com>
2022-11-07 23:06:52 -0300

    feat: add new profile translation

--- resources/js/Locales/dictionary/ptbr.js
+++ resources/js/Locales/dictionary/ptbr.js
@@ -1,5 +1,12 @@
  const en = {
     words: {
+
+        save: 'Salvar',
+        saved: 'Salvou.',
+        enable: 'Permitir',
+        confirm: 'Confirmar',
+        done: 'Feito.',
+
         // Contractor
         contractor: 'Contratante',
 
@@ -58,7 +65,7 @@
         email: 'E-mail',
         password: 'Senha',
         fullName: 'Nome completo',
-        confirmPassword: 'Confirmar senha',
+        confirmPassword: 'Confirmar Senha',
         resetPassword: 'Alterar senha',
         new: 'Novo',
         close: 'Fechar',
@@ -68,8 +75,55 @@
         certificationName: 'Nome do certificado',
         certificationIdentificator: 'Identificador do certificado',
         userSettings: 'Configurações do usuário',
+
+        //Profile Information
+        profileInformation: 'Informação do Perfil',
+
+        //Update Password
+        updatePassword: 'Atualizar senha',
+        currentPassword: 'Senha atual',
+        newPassword: 'Nova Senha',
+
+        //Two Factor Authentication
+        twoFactorAuthentication: 'Autenticação de dois fatores',
+        setupKey: 'Chave de configuração',
+        code: 'Código',
+        disable: 'Desativar',
+
+        //Browser Sessions
+        browserSessions: 'Sessões do navegador',
+        thisDevice: 'Este aparelho',
+
     },
-    phrases: {},
+    phrases: {
+
+        //Profile Information
+        updateInformation: 'Atualize as informações de perfil e o endereço de e-mail da sua conta.',
+        unverifiedEmail:'Seu endereço de e-mail não foi verificado.',
+        clickHereEmail: 'Clique aqui para reenviar o e-mail de verificação.',
+        newVerificationLink: 'Um novo link de verificação foi enviado para seu endereço de e-mail.',
+
+        //Update Password
+        ensureYourAccount: 'Certifique-se de que sua conta esteja usando uma senha longa e aleatória para se manter seguro.',
+
+        //Two Factor Authentication
+        addAdditionalSecurity: 'Adicione segurança adicional à sua conta usando autenticação de dois fatores.',
+        factorAuthentication: 'Você não ativou a autenticação de dois fatores.',
+        finishEnablingAuthentication: 'Termine de habilitar a autenticação de dois fatores.',
+        whenTwoFactorAuthentication: 'Quando a autenticação de dois fatores estiver habilitada, você será solicitado a fornecer um token seguro e aleatório durante a autenticação. Você pode recuperar esse token do aplicativo Google Authenticator do seu telefone.',
+        toFinishEnabling: 'Para concluir a ativação da autenticação de dois fatores, digitalize o código QR a seguir usando o aplicativo autenticador do seu telefone ou insira a chave de configuração e forneça o código OTP gerado.',
+        twoFactorAuthenticationIsNowEnabled: 'A autenticação de dois fatores agora está habilitada. Digitalize o código QR a seguir usando o aplicativo autenticador do seu telefone ou digite a chave de configuração.',
+        storeTheseRecoveryCodes: 'Armazene esses códigos de recuperação em um gerenciador de senhas seguro. Eles podem ser usados ​​para recuperar o acesso à sua conta se seu dispositivo de autenticação de dois fatores for perdido.',
+        regenerateRecoveryCodes: 'Regenerar Códigos de Recuperação',
+        showRecoveryCodes: 'Mostrar códigos de recuperação',
+
+        //Browser Sessions
+        manageSessions: 'Gerencie e desconecte suas sessões ativas em outros navegadores e dispositivos.',
+        ifNecessary: 'Se necessário, você pode sair de todas as outras sessões do navegador em todos os seus dispositivos. Algumas de suas sessões recentes estão listadas abaixo; no entanto, esta lista pode não estar completa. Se você achar que sua conta foi comprometida, você também deve atualizar sua senha.',
+        lastActive: 'Ativo pela última vez',
+        logOutOtherBrowserSessions: 'Sair de outras sessões do navegador',
+        pleaseEnteryourPassword: 'Digite sua senha para confirmar que deseja sair de suas outras sessões do navegador em todos os seus dispositivos.',
+    },
     messages: {
         priceMeasurement: 'para referência de preço',
         work: {
@@ -108,7 +162,7 @@
             reset: 'E-mail com link para troca de senha',
         },
         work: {
-            hasUnity: 'Work has unity type?',
+            hasUnity: 'O trabalho tem tipo de unidade?',
         },
     },
 };
