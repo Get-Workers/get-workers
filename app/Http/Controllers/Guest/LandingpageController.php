a502b4f9f17eab0cd3a7ff9f52701f328887e22f
Haaragard <diogololz159@gmail.com>
2022-11-09 00:45:51 -0300

    feat: add new ladingpage controller

---
+++ app/Http/Controllers/Guest/LandingpageController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Services\Caches\WorkCacheService;
+use Illuminate\Http\Request;
+use Inertia\Response;
+
+class LandingpageController extends Controller
+{
+    public function __invoke(Request $request): Response
+    {
+        $works = WorkCacheService::listPaginate(
+            perPage: 21,
+            actualPage: $request->input('page', 1),
+            filters: ['moreHires'],
+            with: ['unity', 'worker.user']
+        );
+
+        return inertia('Landingpage', compact('works'));
+    }
+}
