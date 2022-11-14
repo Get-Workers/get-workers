efe141c173f9e28113386c3bac2b7e7e20697c6d
Haaragard <diogololz159@gmail.com>
2022-11-14 16:21:14 -0300

    feat: add information icon on label component

--- resources/js/Components/Label.vue
+++ resources/js/Components/Label.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { Information } from 'mdue';
+
 defineProps({
     value: String,
     required: {
@@ -8,14 +10,20 @@ defineProps({
     optional: {
         type: Boolean,
         default: false
-    }
+    },
+    info: {
+        type: String,
+        required: false,
+        default: '',
+    },
 });
 </script>
 
 <template>
-    <label class="block font-medium text-sm text-gray-700">
+    <label class="flex font-medium text-sm text-gray-700">
         <span v-if="value">{{ value }}</span>
         <span v-else><slot /></span>
+        <span class="ml-1 text-indigo-500" v-if="info.length" :title="info"><Information /></span>
         <span class="ml-1 text-red-400" v-if="required && !optional">*</span>
         <em class="ml-1 text-gray-500" v-if="optional && !required">(optional)</em>
     </label>
