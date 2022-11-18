b96e22480dd638c8f3e90e3117df92951eaa7f73
Haaragard <diogololz159@gmail.com>
2022-11-18 00:28:16 -0300

    feat: add update worker phone numbers request

---
+++ app/Http/Requests/User/Worker/UpdateWorkerPhoneNumbersRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\User\Worker;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateWorkerPhoneNumbersRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return (! is_null(auth()->user()->worker));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'phoneNumbers' => 'nullable|array|max:3',
+            'phoneNumbers.*.number' => [
+                'required',
+                'string',
+                'min:15',
+                'max:15',
+                'distinct',
+            ],
+        ];
+    }
+}
