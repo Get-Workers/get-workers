7a7cf2a11873510c1080cd2a1d2cca3c32e08546
Haaragard <diogololz159@gmail.com>
2022-11-18 22:05:35 -0300

    chore: fix things

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,7 +25,6 @@ class DatabaseSeeder extends Seeder
             SpecialtyWorkerSeeder::class,
             SpecialtyWorkSeeder::class,
             PhoneNumberSeeder::class,
-            PhoneNumberWorkerSeeder::class,
         ]);
     }
 }
