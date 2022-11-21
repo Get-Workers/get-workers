4a53bb77eaa197529bc6cbc40dca529bf004ba12
Haaragard <diogololz159@gmail.com>
2022-11-21 19:44:58 -0300

    feat: add review table, model, factory and seeder

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->title(),
+            'description' => $this->faker->text(),
+            'rating' => random_int(0, 5),
+        ];
+    }
+}
