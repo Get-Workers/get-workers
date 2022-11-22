1b1cecdd8fa32fe87d6603b824720e4ec23a5d5c
Haaragard <diogololz159@gmail.com>
2022-11-21 22:58:36 -0300

    feat: add star rating vue component

---
+++ resources/js/Components/Ratings/StarRating.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { StarOutline as StarEmpty, Star } from 'mdue';
+
+defineProps({
+    modelValue: {
+        type: Number,
+        default: 0,
+    },
+    quantity: {
+        type: Number,
+        default: 5,
+    },
+});
+
+defineEmits(['update:modelValue']);
+</script>
+
+<template>
+    <div class="flex w-fit space-x-2 p-2 border rounded text-xl">
+        <div class="relative w-5 h-5 hover:cursor-pointer" v-for="number in quantity" :key="`rating-start-${number}`" @click="$emit('update:modelValue', number)">
+            <Star :class="{ 'hidden hover:block': (modelValue < number) }" />
+            <StarEmpty class="absolute inset-0" />
+        </div>
+    </div>
+</template>
