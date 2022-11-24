1b86ae293ff853c45a01f73b05ea2993e5d0ce95
Haaragard <diogololz159@gmail.com>
2022-11-23 21:44:29 -0300

    feat: change way to delete hired works

--- app/Http/Controllers/User/Profile/Worker/HiredWork/DestroyHiredWorksController.php
+++ app/Http/Controllers/User/Profile/Worker/HiredWork/DestroyHiredWorksController.php
@@ -22,19 +22,13 @@ class DestroyHiredWorksController extends Controller
     {
         $worker = auth()->user()->worker;
         $hiredWorkUuid = $request->validated('hiredWork');
-        $hiredWork = HiredWork::whereUuid($hiredWorkUuid)
+        HiredWork::whereUuid($hiredWorkUuid)
             ->with([
                 'work' => fn ($query) => $query->withTrashed(),
-                'work.worker'
-            ]);
-        if (is_null($hiredWork)) {
-            abort(Response::HTTP_NOT_FOUND);
-        }
-        $hiredWork->delete();
-
-        HiredWorkCacheService::findUuid($hiredWorkUuid, clear: true);
-        HiredWorkCacheService::fromContractor($hiredWork->contractor, clear:true);
-        HiredWorkCacheService::fromWorker($worker, clear:true);
+                'work.worker' => fn ($query) => $query->whereKey($worker->id),
+            ])
+            ->firstOrFail()
+            ->delete();
 
         return redirect()->route('user.profile.worker.hired-works.list')->with('destroy', true);
     }
