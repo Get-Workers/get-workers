ecad19ddc3f8ad40fb95cc62321a19079a77d711
Haaragard <diogololz159@gmail.com>
2022-11-23 21:48:53 -0300

    fix: change pages spacing

--- resources/js/Pages/Landingpage.vue
+++ resources/js/Pages/Landingpage.vue
@@ -9,16 +9,18 @@ defineProps({ works: Object });
 <template>
     <GuestLayout :title="$t('phrases.mostHiredWorks')">
         <template #main>
-            <div class="min-h-full">
-                <h1 class="font-bold text-lg">{{ $t('phrases.mostHiredWorks') }}</h1>
+            <div class="flex px-5">
+                <div class="w-full min-h-full">
+                    <h1 class="font-bold text-lg">{{ $t('phrases.mostHiredWorks') }}</h1>
 
-                <div class="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-5 lg:gap-7 w-fit mx-auto mt-5">
-                    <template v-for="work in works.data" :key="`card-list-${work.uuid}`">
-                        <WorkCard :work="work"/>
-                    </template>
+                    <div class="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-5 lg:gap-7 w-fit mx-auto mt-5">
+                        <template v-for="work in works.data" :key="`card-list-${work.uuid}`">
+                            <WorkCard :work="work"/>
+                        </template>
+                    </div>
+
+                    <Pagination class="mt-2" :links="works.links" />
                 </div>
-
-                <Pagination class="mt-2" :links="works.links" />
             </div>
         </template>
     </GuestLayout>
