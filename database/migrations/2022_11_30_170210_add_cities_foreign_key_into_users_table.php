7075c914f15bcea209984cfcdb920884789c3748
Haaragard <diogololz159@gmail.com>
2022-11-30 17:09:32 -0300

    feat: add city reference on users table

---
+++ database/migrations/2022_11_30_170210_add_cities_foreign_key_into_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\City;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(City::class)->nullable()->after('current_team_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropConstrainedForeignIdFor(City::class);
+        });
+    }
+};
